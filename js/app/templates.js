c43c15c1ba9825ecf6e10d0c010026ef575b3ab0
Mike Gioia <mikegioia@gmail.com>
2015-04-13 18:44:48 -0400

    Removing templates file

--- js/app/templates.js
+++
@@ -1,4 +0,0 @@
-this["App"] = this["App"] || {};
-this["App"]["Templates"] = this["App"]["Templates"] || {};
-
-this["App"]["Templates"]["./js/app/views/sample.html"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div>\n    ' +((__t = ( name )) == null ? '' : __t) +', what is your name?\n</div>';}return __p};
